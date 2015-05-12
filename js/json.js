4331278e2e15cfeef8b6bff1b489cda0b8347686
Mackoo <mackoo@opoczta.pl>
2015-05-12 11:02:04 +0200

    added json.js

---
+++ js/json.js
@@ -0,0 +1,22 @@
+work = {
+"jobs":[{
+"employer":"Orange","title":"developer","location":"wawa",
+    "dates":"2015","description":"llorem"}
+]
+}
+
+projects={
+"projects":[{
+"title":"developer","images":"img.png",
+    "dates":"2015","description":"llorem"}
+]
+}
+
+bio={
+"name":"MO","role":"","welcomeMessage":"","contacts":{},"skills":["skill1","skill2"]
+}
+
+education ={
+    "schools":[{}],
+    "onlineCourses":[{}]
+}
